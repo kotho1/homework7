7832fabffddff5b2348f4847b7cae85ce2535ef1
kotho1 <thompsonkate10@gmail.com>
2020-12-03 15:42:40 -0600

    finished product

--- js/weather.js
+++ js/weather.js
@@ -1,31 +1,44 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.getElementById("forecast").style.display = "block";
 
     //Set default location if one isn't provided
-    let location;
+    let location = document.querySelector("#location").value || "Ann Arbor";
 
     // Your code here.
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    let fomat;
+    let unitInput = document.getElementsByName("temp");
+    let format = unitInput[1].checked ? unitInput[1].value : unitInput[0].value;
+
+    // let format = document.querySelectorAll("input[name=temp]:checked").value || "imperial";
 
     // Your code here.
-    console.log("Format is " + format);
+    console.log("Format is : " + format);
 
     //set the query  
-    let query;
+    let query = "https://api.openweathermap.org/data/2.5/weather?";
     // Your code here.  
+    if (/\d/.test(parseInt(Array.from(location)[0]), 10)) {
+        query += "zip=" + location;
+    }
+
+    else {
+        query += "q=" + location;
+    }
+
+    query += "&units=" + format + "&appid=" + "970aa03a90f28399253607178b53951c";
     
     console.log("Query is :" + query);
 
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
 
-    let loc;
-    let temp;
-    let tempImg;
+    let loc = document.querySelector("#loc");
+    let temp = document.querySelector("#temp"); 
+    let tempImg = document.querySelector("#tempImg");
     // Your code here.
 
 
@@ -34,6 +47,10 @@ function gettingJSON(){
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
-
+        console.log(JSON.stringify(json));
+        loc.innerHTML = json.name;
+        temp.innerHTML = json.main.temp + " with " + json.weather[0].description;
+        tempImg.src = " http://openweathermap.org/img/wn/" + json.weather[0].icon + ".png";
+        tempImg.alt = json.weather[0].description;
     });
 }
